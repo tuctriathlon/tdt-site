2440a7ea34d2144b06701978ee85179b7fd8a009
Francis Tosolini <tosolini@gutenberg-technology.com>
2024-08-19 16:32:44 +0200

    fix: routing

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -45,6 +45,7 @@ const routes: Routes = [
                     {
                         path: '',
                         redirectTo: 'programme',
+                        pathMatch: 'full',
                     },
                 ],
             },
