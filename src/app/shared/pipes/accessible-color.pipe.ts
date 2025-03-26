a8fd15220d8cbb48f5df8f44babae31e29e88a28
Francis Tosolini <tosolini@gutenberg-technology.com>
2025-03-26 10:24:33 +0100

    chore: test theme 2025

---
+++ src/app/shared/pipes/accessible-color.pipe.ts
@@ -0,0 +1,67 @@
+import { AfterViewInit, Directive, ElementRef, OnInit, Pipe, PipeTransform, Renderer2 } from '@angular/core'
+import { AccessibilityUtils } from 'src/app/shared/services/accessibility.utils'
+import { DataService } from 'src/app/shared/services/data.service'
+
+@Directive( {
+    standalone: true,
+    selector: '[accessibleColor]',
+})
+export class AccessibleColorDirective implements AfterViewInit{
+    constructor(
+        private el: ElementRef,
+        private dataService: DataService
+    ) {
+        el.nativeElement.style.customProperty = true
+    }
+
+    ngAfterViewInit() {
+        const backgroundColor = this.getBackgroundColor()
+        const initialTextColor = this.getTextColor()
+        console.log(backgroundColor,initialTextColor, AccessibilityUtils.computeContrastRatio(backgroundColor, initialTextColor).toString())
+        if (this.isAccessibleColor(backgroundColor, initialTextColor)) {
+            return
+        }
+        // console.log(backgroundColor, this.textColors.map((color) => ({ color, ratio: AccessibilityUtils.computeContrastRatio(backgroundColor, color) })))
+        const textColor = this.textColors.map((color) => ({ color, ratio: AccessibilityUtils.computeContrastRatio(backgroundColor, color) }))
+            .reduce((acc, val) => acc.ratio > val.ratio ? acc : val, { color: '#00aaff', ratio: 0 }).color
+        // console.log(textColor, backgroundColor)
+        this.el.nativeElement.style.color = textColor
+    }
+
+    get textColors(): string[] {
+        return [
+            this.dataService.config.value.primary_color,
+            this.dataService.config.value.primary_color,
+            '#000000',
+            '#FFFFFF'
+        ]
+    }
+
+    /**
+     * Get the background color of the element
+     * @private
+     */
+    private getBackgroundColor(): string {
+        const computedStyle = window.getComputedStyle(this.el.nativeElement)
+        return computedStyle.backgroundColor
+    }
+
+    /**
+     * Get the text color of the element
+     * @private
+     */
+    private getTextColor(): string {
+        const computedStyle = window.getComputedStyle(this.el.nativeElement)
+        return computedStyle.color
+    }
+
+    /**
+     * Return is the color contrast is accessible
+     * @param bgColor the background color
+     * @param textColor the color to check
+     * @private
+     */
+    private isAccessibleColor(bgColor: string, textColor: string): boolean {
+        return AccessibilityUtils.computeContrastRatio(bgColor, textColor) >= 4.5 // WCAG AA
+    }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { FooterComponent } from 'src/app/shared/components/footer/footer.compone
 import { NavbarComponent } from 'src/app/shared/components/navbar/navbar.component'
 import { SlideshowComponent } from 'src/app/shared/components/slideshow/slideshow.component'
 import { SubmenuComponent } from 'src/app/shared/components/submenu/submenu.component'
+import { AccessibleColorDirective } from 'src/app/shared/pipes/accessible-color.pipe'
 import { OrderByPipe } from 'src/app/shared/pipes/orderBy.pipe'
 
 import { SafeHtmlPipe } from 'src/app/shared/pipes/safe-html.pipe'
@@ -14,7 +15,7 @@ import { SafeUrlPipe } from 'src/app/shared/pipes/safe-url.pipe'
 import { DataService } from 'src/app/shared/services/data.service'
 
 @NgModule({
-    imports: [CommonModule, RouterModule, NgOptimizedImage],
+    imports: [CommonModule, RouterModule, NgOptimizedImage, AccessibleColorDirective],
     exports: [
         NavbarComponent,
         SlideshowComponent,
